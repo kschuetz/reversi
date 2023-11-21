10331079720c46fdf72d920dba5e88fa8cca726c
Kevin Schuetz <schuetzk@gmail.com>
2023-11-20 21:11:31 -0600

    Begin work on PhysicalBoard

---
+++ src/main/scala/reversi/ui/components/PhysicalBoard.scala
@@ -0,0 +1,35 @@
+package reversi.ui.components
+
+import com.raquo.laminar.api.L.svg.*
+import com.raquo.laminar.nodes.ReactiveSvgElement
+import org.scalajs.dom.SVGGElement
+
+object PhysicalBoard {
+
+  inline def SquareSize: Double = 1.0
+
+  private inline def SquareSizeStr = SquareSize.toString
+
+  val squareCenterOffset: Double = SquareSize / 2
+  private val boardCenterOffset = 3.5 * SquareSize
+
+  object Css {
+    val boardSquare = "board-square"
+  }
+
+}
+
+final class PhysicalBoard {
+
+  import PhysicalBoard.*
+
+  def apply(): ReactiveSvgElement[SVGGElement] = g()
+
+  private def Square(centerX: Double, centerY: Double) =
+    rect(className := Css.boardSquare,
+      x := (centerX - squareCenterOffset).toString,
+      y := (centerY - squareCenterOffset).toString,
+      width := SquareSizeStr,
+      height := SquareSizeStr,
+    )
+}
