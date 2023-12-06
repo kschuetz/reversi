d6843c2924a6e50d5e243f75db0ec887b7877b70
Kevin Schuetz <schuetzk@gmail.com>
2023-12-06 00:31:45 -0600

    Basic physical board

--- src/main/scala/reversi/ui/board/PhysicalBoard.scala
+++ src/main/scala/reversi/ui/board/PhysicalBoard.scala
@@ -1,5 +1,6 @@
 package reversi.ui.board
 
+import com.raquo.laminar.api.L.seqToModifier
 import com.raquo.laminar.api.L.svg.*
 import com.raquo.laminar.nodes.ReactiveSvgElement
 import org.scalajs.dom.{SVGElement, SVGGElement}
@@ -10,11 +11,12 @@ object PhysicalBoard {
 
   private inline def SquareSizeStr = SquareSize.toString
 
-  val squareCenterOffset: Double = SquareSize / 2
-  private val boardCenterOffset = 3.5 * SquareSize
+  val SquareCenterOffset: Double = SquareSize / 2
+  private val BoardCenterOffset = 3.5 * SquareSize
 
   object Css {
     val boardSquare = "board-square"
+    val boardBorder = "board-border"
   }
 
 }
@@ -23,13 +25,34 @@ final class PhysicalBoard {
 
   import PhysicalBoard.*
 
-  def apply(): ReactiveSvgElement[SVGElement] = g()
+  def apply(): ReactiveSvgElement[SVGElement] = {
+    val squares = for {
+      row <- 0 to 7
+      centerY = row - BoardCenterOffset
+      col <- 0 to 7
+      centerX = col - BoardCenterOffset
+    } yield Square(centerX, centerY)
+    g(BoardBorder(0.3),
+      seqToModifier(squares))
+  }
 
   private def Square(centerX: Double, centerY: Double) =
     rect(className := Css.boardSquare,
-      x := (centerX - squareCenterOffset).toString,
-      y := (centerY - squareCenterOffset).toString,
+      x := (centerX - SquareCenterOffset).toString,
+      y := (centerY - SquareCenterOffset).toString,
       width := SquareSizeStr,
       height := SquareSizeStr,
     )
+
+  private def BoardBorder(thickness: Double) = {
+    val origin = -4 - thickness
+    val w = 8 + 2 * thickness
+    rect(className := Css.boardBorder,
+      x := origin.toString,
+      y := origin.toString,
+      width := w.toString,
+      height := w.toString,
+    )
+  }
+
 }
